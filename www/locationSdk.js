09072d514e4e9795302625db385b73db79d31f7d
Cundong Zhang <155466@qq.com>
2020-02-21 14:56:15 +0800

    Optimize js file export module

--- www/locationSdk.js
+++ www/locationSdk.js
@@ -1,18 +1,21 @@
 /*global cordova, module*/
 var exec = require('cordova/exec');
 
-var LocationSdk = function () { };
+var LocationSdkOriginal = (function () {
+    /** Initilize the location sdk */
+    LocationSdk.prototype.init = function (successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "LocationSdk", "init");
+    };
+    /** Invoke start operation */
+    LocationSdk.prototype.start = function (successCallback, errorCallback, shippingNoteInfos) {
+        exec(successCallback, errorCallback, "LocationSdk", "start", [shippingNoteInfos]);
+    };
+    /** Invoke stop operation */
+    LocationSdk.prototype.stop = function (successCallback, errorCallback, shippingNoteInfos) {
+        exec(successCallback, errorCallback, "LocationSdk", "stop", [shippingNoteInfos]);
+    };
+})();
 
-LocationSdk.prototype.init = function (successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "LocationSdk", "init");
-};
+var LocationSdk = new LocationSdkOriginal();
 
-LocationSdk.prototype.start = function (successCallback, errorCallback, shippingNoteInfos) {
-    exec(successCallback, errorCallback, "LocationSdk", "start", [shippingNoteInfos]);
-};
-
-LocationSdk.prototype.stop = function (successCallback, errorCallback, shippingNoteInfos) {
-    exec(successCallback, errorCallback, "LocationSdk", "stop", [shippingNoteInfos]);
-};
-
-module.exports = LocationSdk;
+export { LocationSdk };
